3289e96528f23376113e43201b4088170e1cd170
slamix <lokomotivsapsan@gmail.com>
2025-05-14 15:40:13 +0300

    disable linter of hexlet-check

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App.jsx';
-import store from './slices/index.js';
-import './utils/i18n.js';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import App from './App.jsx'
+import store from './slices/index.js'
+import './utils/i18n.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
